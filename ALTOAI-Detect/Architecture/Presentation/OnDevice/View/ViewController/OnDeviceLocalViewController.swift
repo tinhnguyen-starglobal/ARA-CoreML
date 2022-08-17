ca4fd699876e177375c5a5ccc17c06a1c6aa0f0d
Tinh Nguyen <tinh.nguyen@star.global>
2022-08-18 02:49:11 +0700

    feat: managing viewo controller with container view controller

---
+++ ALTOAI-Detect/Architecture/Presentation/OnDevice/View/ViewController/OnDeviceLocalViewController.swift
@@ -0,0 +1,15 @@
+//
+//  OnDeviceLocalViewController.swift
+//  ALTOAI-Detect
+//
+//  Created by Tinh Nguyen on 18/08/2022.
+//
+
+import UIKit
+
+final class OnDeviceLocalViewController: BaseViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ ALTOAI-Detect/Architecture/Presentation/OnDevice/View/ViewController/OnDeviceRemoteViewController.swift
@@ -0,0 +1,15 @@
+//
+//  OnDeviceRemoteViewController.swift
+//  ALTOAI-Detect
+//
+//  Created by Tinh Nguyen on 18/08/2022.
+//
+
+import UIKit
+
+final class OnDeviceRemoteViewController: BaseViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
