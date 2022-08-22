eb23f499f02367f18b956b4a8536ede1faa28cd7
Tinh Nguyen <tinh.nguyen@star.global>
2022-08-23 01:52:13 +0700

    fix: update authen with refresh token

--- ALTOAI-Detect/Model/AccessToken.swift
+++ ALTOAI-Detect/Model/AccessToken.swift
@@ -8,11 +8,16 @@
 import Foundation
 
 struct AccessToken: Decodable {
-  let accessToken: String
-  let expiresIn: Int
-
-  enum CodingKeys: String, CodingKey {
-    case accessToken = "access_token"
-    case expiresIn = "expires_in"
-  }
+    
+    let accessToken: String
+    let expiresIn: Int
+    var refreshToken: String
+    var refreshExpiresIn: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case accessToken = "access_token"
+        case expiresIn = "expires_in"
+        case refreshToken = "refresh_token"
+        case refreshExpiresIn = "refresh_expires_in"
+    }
 }
